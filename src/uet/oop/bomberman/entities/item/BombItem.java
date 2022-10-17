14a923baaf1ee61745cced19119ae2dfcf45adcb
hanguyenanhson <hanguyenanhson@gmail.com>
2022-10-17 14:16:49 +0700

    maybe

---
+++ src/uet/oop/bomberman/entities/item/BombItem.java
@@ -0,0 +1,15 @@
+package uet.oop.bomberman.entities.item;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.entities.item.Item;
+
+public class BombItem extends Item {
+    public BombItem(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+    @Override
+    public void update() {
+
+    }
+}
